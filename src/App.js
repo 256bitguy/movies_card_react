39e8fd59519c2864342be8453eae5ae0f9aff373
256bitguy <ashishrajsri2233@gmail.com>
2023-05-21 14:55:56 +0530

    changed files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ShowList from './ShowList';
+import ShowSummary from './ShowSummary';
+import Formed from './form';
+import Modal from 'react-modal'
+ 
 
-function App() {
+const App = () => {
+  
+
+
+  const [selectedShow, setSelectedShow] = useState(null);
+  const [bookingDetails, setBookingDetails] = useState(null);
+
+  const handleSelectShow = (show) => {
+    setSelectedShow(show);
+  };
+
+  const handleBookTicket = () => {
+    setBookingDetails({
+      showName: selectedShow.name,
+    });
+  };
+
+  const handleBackToShows = () => {
+    setSelectedShow(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+      {selectedShow ? (
+        <div>
+          <button onClick={handleBackToShows}>Back to Shows</button>
+          <ShowSummary show={selectedShow} onBookTicket={handleBookTicket} />
+        </div>
+      ) : (
+        <ShowList onSelectShow={handleSelectShow} />
+      )}
+       
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/form.js
@@ -0,0 +1,68 @@
+import React, { useState ,useEffect} from 'react';
+
+const FormComponent=()=> {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [formData, setFormData] = useState(null);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Save form data to local storage
+    const formData = {
+      name,
+      email,
+    };
+
+    // Convert the form data to a JSON string
+    const formDataJson = JSON.stringify(formData);
+
+    // Save the JSON string to local storage
+    localStorage.setItem('formData', formDataJson);
+
+    // Reset form fields
+    setName('');
+    setEmail('');
+  };
+  useEffect(() => {
+    // Retrieve the form data from local storage
+    const formDataJson = localStorage.getItem('formData');
+
+    if (formDataJson) {
+      // Convert the JSON string back to an object
+      const formDataObj = JSON.parse(formDataJson);
+      setFormData(formDataObj);
+    }
+  }, []);
+
+ const display=()=>{
+  alert(`${formData.name} you have successfully booked the ticket`);
+ }
+
+   
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={handleNameChange} />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input type="email" value={email} onChange={handleEmailChange} />
+      </label>
+      <br />
+      <button type="submit" onSubmit={display}>Submit</button>
+    </form>
+  );
+}
+
+export default FormComponent;
